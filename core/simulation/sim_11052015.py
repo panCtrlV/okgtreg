8533be75dc1bb836173cf9edffe5b7b163147d02
Pan Chao <panchao25@gmail.com>
2015-11-05 10:06:12 -0500

    Added step-wise forward selection algorithm as a function into a separate "forwardSelection.py". Added a simulation to show how to use the function.

---
+++ core/simulation/sim_11052015.py
@@ -0,0 +1,41 @@
+from core.forwardSelection import *
+from core.DataSimulator import *
+
+"""
+An example of OKGT group structure determination by
+steop-wise forward selection.
+
+In this example, we choose a model which is fully additive,
+i.e. no interactions.
+"""
+
+# Simulation data
+np.random.seed(25)
+y, X = DataSimulator.SimData_Wang04(1000)  # Simulate data
+data = Data(y, X)
+
+kernel = Kernel('gaussian', sigma=0.5)
+
+# Step-wise foward selection to determine group structure
+res = forwardSelection(data, kernel, useLowRankApproximation=True, rank=10)
+
+"""Simulation result:
+
+In this example, we successfully recover the true group structure:
+
+    ** SELECTED GROUP STRUCTURE: ([1], [2], [3], [4], [5])
+
+Note: The algorithm is tested under two setting:
+
+        1) enabling low rank approximation (using Nystroem method),
+        2) no low rank approximation.
+
+      The performance of the algorithm under the two settings are different.
+      It seems that the detected structure from the first setting is closer
+      to the true structure than that from the second setting.
+
+      It was also noticed that the sample size affects the final result.
+
+      Using low rank approximation also improve the computation speed of the
+      algorithm.
+"""
