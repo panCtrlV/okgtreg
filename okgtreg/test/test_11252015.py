8965d7cbe12e775d22be2f762975d85e672f1722
Pan Chao <panchao25@gmail.com>
2015-11-25 17:23:08 -0500

    Added a single train method in OKGTReg class to unify different training methods.

--- okgtreg/test/test_11252015.py
+++ okgtreg/test/test_11252015.py
@@ -2,9 +2,17 @@ from okgtreg.DataSimulator import DataSimulator
 from okgtreg.Group import Group
 from okgtreg.Kernel import Kernel
 from okgtreg.Parameters import Parameters
+from okgtreg.OKGTReg import OKGTReg
 
 
 data = DataSimulator.SimData_Wang04WithInteraction(100)
 group = Group([1], [2], [3], [4], [5], [6, 7])
 kernel = Kernel('gaussian', sigma=0.5)
 parameters = Parameters(group, kernel, [kernel]*group.size)
+okgt = OKGTReg(data, parameters)
+
+res1 = okgt.train(method='vanilla')
+res1
+
+res2 = okgt.train(method='nystroem')
+res2
