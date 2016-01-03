a132ab28576183957e9f7b119c26ebc6e91825bc
Pan Chao <panchao25@gmail.com>
2016-01-03 10:54:57 +0800

    Added calculation for mean of R2.

--- okgtreg/simulation/sim_01032016/sim_splitAndMerge_report.py
+++ okgtreg/simulation/sim_01032016/sim_splitAndMerge_report.py
@@ -8,5 +8,7 @@ pklfile = open('okgtreg/simulation/sim_01032016/sim_splitAndMerge.py.pkl', 'rb')
 groups, r2s = pickle.load(pklfile)
 pklfile.close()
 
+np.mean(r2s)
+
 printGroupingFrequency(groups)
 printGroupFrequency(groups)
