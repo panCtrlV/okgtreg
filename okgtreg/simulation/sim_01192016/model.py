daf8ebbb88e9414a55b9e7a5033212c35d4d36e0
Pan Chao <panchao25@gmail.com>
2016-01-19 08:21:26 -0500

    Refined the model.

--- okgtreg/simulation/sim_01192016/model.py
+++ okgtreg/simulation/sim_01192016/model.py
@@ -8,16 +8,16 @@ absolute functions.
 '''
 
 import numpy as np
-from scipy.special import expit
+from scipy.special import expit, cbrt
 from okgtreg import *
 
 
-def simpleData(n):
+def simpleData2(n):
     '''
     An simple model whose data is used in the current
     simulation. The model is:
 
-        y = ( sin(x_1) + log(x_2 + x_3) + sigmoid(x_1 + x_2 + x_3) + \epsilon )^2
+        y = ( sin(x_1) + log(|x_2 + x_3|) + sigmoid(x_1 + x_2 + x_3) + \epsilon )^(1/3)
 
         x_i \sim Unif(-1, 1)
         \epsilon \sim N(0, 0.1^2)
@@ -25,8 +25,21 @@ def simpleData(n):
     :param n:
     :return:
     '''
-    x = np.random.uniform(-1., 1., (n, 6))
+    x = np.random.normal(0., 1., (n, 6))
     e = np.random.normal(0., 0.1, n)
-    y = (np.sin(x[:, 0]) + np.log(x[:, 1] * x[:, 2]) +
-         expit(x[:, 3] * x[:, 4] * x[:, 5]) + e) ** 2
+    y = cbrt(np.sin(x[:, 0]) +
+             np.log(np.abs(x[:, 1] * x[:, 2])) +
+             expit(x[:, 3] * x[:, 4] * x[:, 5]) +
+             e)
     return Data(y, x), Group([1], [2, 3], [4, 5, 6])
+
+
+if __name__ == '__main__':
+    np.random.seed(25)
+    n = 500
+    data, group = simpleData2(n)
+
+    import matplotlib.pyplot as plt
+
+    plt.scatter(np.arange(n), data.y)  # scatter plot
+    plt.hist(data.y, 30)  # histogram shows bi-modal
