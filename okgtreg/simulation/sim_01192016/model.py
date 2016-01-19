04f9a7b7dc2b2476c511602c56b615289a86f64e
Pan Chao <panchao25@gmail.com>
2016-01-19 09:24:59 -0500

    Corrected model.

--- okgtreg/simulation/sim_01192016/model.py
+++ okgtreg/simulation/sim_01192016/model.py
@@ -28,8 +28,8 @@ def simpleData_01192016(n):
     x = np.random.normal(0., 1., (n, 6))
     e = np.random.normal(0., 0.1, n)
     y = cbrt(np.sin(x[:, 0]) +
-             np.log(np.abs(x[:, 1] * x[:, 2])) +
-             expit(x[:, 3] * x[:, 4] * x[:, 5]) +
+             np.log(np.abs(x[:, 1] + x[:, 2])) +
+             expit(x[:, 3] + x[:, 4] + x[:, 5]) +
              e)
     return Data(y, x), Group([1], [2, 3], [4, 5, 6])
 
@@ -42,4 +42,4 @@ if __name__ == '__main__':
     import matplotlib.pyplot as plt
 
     plt.scatter(np.arange(n), data.y)  # scatter plot
-    plt.hist(data.y, 30)  # histogram shows bi-modal
+    plt.hist(data.y, 50)  # histogram shows bi-modal
