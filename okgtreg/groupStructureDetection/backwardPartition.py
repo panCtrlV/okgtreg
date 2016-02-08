de89219c347fd002b412b0b8f97ab149cce54f08
Pan Chao <panchao25@gmail.com>
2016-02-07 22:46:17 -0500

    Added a second backward method where OKGT is fitted while the response function is known and given.

--- okgtreg/groupStructureDetection/backwardPartition.py
+++ okgtreg/groupStructureDetection/backwardPartition.py
@@ -52,7 +52,7 @@ def rkhsCapacity(group, alpha):
 # Start forward selection
 # ------------------------
 def backwardPartition(data, kernel, method='vanilla', rank=10, seed=None,
-                      mu=1e-5, alpha=np.e):
+                      mu=1e-4, alpha=np.e):
     '''
 
     :param data:
@@ -185,6 +185,143 @@ def backwardPartition(data, kernel, method='vanilla', rank=10, seed=None,
     return dict(group=oldGroup, r2=bestR2)
 
 
+def backwardPartitionWithKnownResponse(data, kernel, g, method='vanilla',
+                                       rank=10, seed=None, mu=1e-4, alpha=np.e):
+    '''
+
+    :param data:
+    :param kernel:
+    :param method:
+    :param rank:
+    :param seed:
+    :param mu: the first tuning parameter for the capacity penalty $\lambda * a^d$
+    :param alpha: the second tuning parameter for capacity penalty in $\lambda * a^d$
+    :return:
+    '''
+    covariatesPool = list(np.arange(data.p) + 1)
+    oldGroup = Group(covariatesPool)  # start with a large single group
+    p = oldGroup.p
+    # bestR2 = 0.
+    bestR2 = -np.inf
+    bestCovariateIndex = None
+
+    counter = 0
+
+    while len(covariatesPool) > 1:
+        counter += 1
+        print("** === Step %d === **" % counter)
+        # Create a new group
+        print("** Create a new group: **")
+        for covariateInd in covariatesPool:
+            print("\t Create a new group for covariate %d ..." % covariateInd)
+            _currentGroup = oldGroup.removeOneCovariate(covariateInd)
+            currentGroup = _currentGroup.addNewCovariateAsGroup(covariateInd)
+            # print("\t\t current group structure: %s " % (currentGroup.getPartitions(),))
+            # Contrary to forward selection, the data matrix doesn't
+            # change.
+            # parameters = Parameters(currentGroup, kernel, [kernel]*currentGroup.size)
+            # currentOKGT = OKGTReg2(data, parameters)
+            currentOKGT = OKGTReg2(data, kernel=kernel, group=currentGroup)
+            # Train OKGT
+            res = currentOKGT._train_Vanilla2(g)
+            # currentR2 = res['r2']
+            # capacity = sum([len(g) ** len(g) for g in currentGroup.partition])
+            capacity = rkhsCapacity(currentGroup, alpha)
+            print("\t\t current group structure: %s with capacity: %.04f" %
+                  (currentGroup.getPartitions(), capacity))
+            currentR2 = res['r2'] - mu * capacity
+            if currentR2 > bestR2:
+                # print("\t\t current R2 =\t %.10f \t *" % currentR2)
+                print("\t\t current R2 (penalized) =\t %.10f \t *" % currentR2)
+                bestR2 = currentR2
+                newGroup = currentGroup
+                bestCovariateIndex = covariateInd
+            else:
+                # print("\t\t current R2 =\t %.10f" % currentR2)
+                print("\t\t current R2 (penalized) =\t %.10f" % currentR2)
+
+            # print("\t\t best R2 =\t\t %.10f \n" % bestR2)
+            print("\t\t best R2 (penalized) =\t\t %.10f \n" % bestR2)
+
+        # print("** Updated group structure is: %s \n" % (newGroup.partition, ))
+        # print '\n'
+        # If there are already new groups, a chosen variable can join one of the
+        # new groups instead of creating a new group.
+        print "** Add to an existing group: **"
+        if oldGroup.size > 1:
+            for covariateInd in covariatesPool:
+                print("\t try adding covariate %d " % covariateInd)
+                # Remove `covariateInd`-th covariate from the pool,
+                # which will be added into one of the other groups.
+                updatedCovariatesPool = copy.deepcopy(covariatesPool)
+                updatedCovariatesPool.remove(covariateInd)
+                # Get the group number of the chosen `covariateInd`
+                covariateMember = oldGroup.getMembership(covariateInd)
+                # Take all other groups as a Group object
+                otherGroupInds = list(np.arange(oldGroup.size) + 1)
+                otherGroupInds.remove(covariateMember)
+
+                # print type(otherGroupInds), ": ", otherGroupInds
+
+                otherGroup = oldGroup.getPartitions(otherGroupInds, True)
+                # Try adding the chosen `covariateInd` to each of the other groups
+                for groupInd in np.arange(otherGroup.size) + 1:
+                    print("\t   in other group %d ..." % groupInd)
+                    updatedOtherGroup = otherGroup.addNewCovariateToGroup(covariateInd, groupInd)
+                    currentGroup = updatedOtherGroup + updatedCovariatesPool
+                    print("\t\t current group structure: %s " % (currentGroup.getPartitions(),))
+                    # parameters = Parameters(currentGroup, kernel, [kernel]*currentGroup.size)
+                    # currentOKGT = OKGTReg2(data, parameters)
+                    currentOKGT = OKGTReg2(data, kernel=kernel, group=currentGroup)
+                    # Train OKGT
+                    # res = currentOKGT.train(method, rank, seed)
+                    res = currentOKGT._train_Vanilla2(g)
+                    # currentR2 = res['r2']
+                    # capacity = sum([len(g) ** len(g) for g in currentGroup.partition])
+                    capacity = rkhsCapacity(currentGroup, alpha)
+                    print("\t\t current group structure: %s with capacity: %.04f" %
+                          (currentGroup.getPartitions(), capacity))
+                    currentR2 = res['r2'] - mu * capacity
+                    # Check if there is improvement
+                    if currentR2 > bestR2:
+                        # print("\t\t current R2 =\t %.10f \t *" % currentR2)
+                        print("\t\t current R2 (penalized) =\t %.10f \t *" % currentR2)
+                        bestR2 = currentR2
+                        newGroup = currentGroup
+                        bestCovariateIndex = covariateInd
+                    else:
+                        # print("\t\t current R2 =\t %.10f" % currentR2)
+                        print("\t\t current R2 (penalized) =\t %.10f" % currentR2)
+
+                    # print("\t\t best R2 =\t\t %.10f \n" % bestR2)
+                    print("\t\t best R2 (penalized) =\t\t %.10f \n" % bestR2)
+        else:
+            print("\t ** No other groups than the pool. Pass ... ** \n")
+
+        print("** Step %d updated group structure is: %s \n" %
+              (counter, newGroup.getPartitions()))
+
+        # print "covariate pool: ", covariatesPool
+        # print "best covariate index so far: ", bestCovariateIndex
+
+        if bestCovariateIndex in covariatesPool:
+            covariatesPool.remove(bestCovariateIndex)
+            oldGroup = newGroup
+            if counter == p - 1:
+                print("** Finish with complete iterations. ** \n")
+        else:
+            print("** Finish with early termination at step %d "
+                  "due to no further improvement of R2. ** \n" % counter)
+            break
+
+    # print ( "** SELECTED GROUP STRUCTURE: %s with R2 = %f ** \n" %
+    #         (oldGroup.getPartitions(), bestR2) )
+    print ("** SELECTED GROUP STRUCTURE: %s with R2 (penalized) = %f ** \n" %
+           (oldGroup.getPartitions(), bestR2))
+    # return dict(group=oldGroup, r2=bestR2)
+    return dict(group=oldGroup, r2=bestR2)
+
+
 if __name__ == "__main__":
     # from okgtreg.DataSimulator import DataSimulator
     from okgtreg.Kernel import Kernel
@@ -207,20 +344,24 @@ if __name__ == "__main__":
     def model10(n):
         x = np.random.uniform(1, 2, (n, 6))
         e = np.random.normal(0., 0.1, n)
-        y = np.log(1. +
-                   np.log(x[:, 0]) +
-                   x[:, 1] / np.exp(x[:, 2]) +
-                   np.power(x[:, 3] + x[:, 4], x[:, 5]) + e)
-        return Data(y, x), Group([1], [2, 3], [4, 5, 6])
+        g = 1. + \
+            np.log(x[:, 0]) + \
+            x[:, 1] / np.exp(x[:, 2]) + \
+            np.power(x[:, 3] + x[:, 4], x[:, 5]) + e
+        y = np.log(g)
+        return Data(y, x), Group([1], [2, 3], [4, 5, 6]), g
 
 
     # Simulate data
     n = 500
     np.random.seed(25)
-    data, tgroup = model10(500)
+    data, tgroup, g = model10(500)
 
     print tgroup
 
     # Run forward inclusion method for group structure detection
     kernel = Kernel('gaussian', sigma=0.5)
     res = backwardPartition(data, kernel)
+
+    # With g known
+    res = backwardPartitionWithKnownResponse(data, kernel, g)
