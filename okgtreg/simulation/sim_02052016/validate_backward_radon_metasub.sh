c4674af7c1f8f29e573e59fd30a44c6a3fa964f4
Pan Chao <panchao25@gmail.com>
2016-02-19 12:24:37 -0500

    Fixed incorrect file name.

--- okgtreg/simulation/sim_02052016/validate_backward_radon_metasub.sh
+++ okgtreg/simulation/sim_02052016/validate_backward_radon_metasub.sh
@@ -4,7 +4,7 @@
 
 for MODEL_ID in 5; do
 	for SEED_NUM in `seq 1 100`; do
- 		 qsub -l procs=1 -l walltime=12:00:00 -v MODEL_ID=$MODEL_ID,SEED_NUM=$SEED_NUM /home/panc/research/OKGT/software/okgtreg/okgtreg/simulation/sim_02052016/validate_radon_sub.sh
+ 		 qsub -l procs=1 -l walltime=12:00:00 -v MODEL_ID=$MODEL_ID,SEED_NUM=$SEED_NUM /home/panc/research/OKGT/software/okgtreg/okgtreg/simulation/sim_02052016/validate_backward_radon_sub.sh
 	done
 done
 
