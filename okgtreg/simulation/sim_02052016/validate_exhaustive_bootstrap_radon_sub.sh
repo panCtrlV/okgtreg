c5f5568efe44333963368bafd715bf067a422715
Pan Chao <panchao25@gmail.com>
2016-03-20 17:46:32 -0400

    Deleted simulation folder after all files are archived.

--- okgtreg/simulation/sim_02052016/validate_exhaustive_bootstrap_radon_sub.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -l
-# FILENAME:  validate_exhaustive_radon_sub.sh
-
-module load anaconda
-cd $PBS_O_WORKDIR
-unset DISPLAY
-
-# This is the path on Radon
-python -u /home/panc/research/OKGT/software/okgtreg/okgtreg/simulation/sim_02052016/validate_exhaustive_bootstrap.py ${MODEL_ID} ${DATA_SEED} ${BT_SEED}
